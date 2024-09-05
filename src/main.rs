aa75b478ccf569d63ef0d215d32b1cc8feb8fd97
Getulio Beninca <getuliovb@gmail.com>
2024-09-05 20:44:28 +0200

    feat: brokenly working file download

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,25 @@
 use axum::{
     Router,
-    extract::Multipart,
-    http::Method,
-    routing::get,
-    routing::put,
-    response::Json
+    http::Method, 
+    response::IntoResponse, 
+    routing::{get, put}, 
+    extract::{Json, Multipart}, 
 };
+use std::fs::File;
 use tower_http::cors::{Any, CorsLayer};
 use std::io::{Cursor, BufReader};
 use serde_json::{json, Value};
-use uesave::{Save};
+use uesave::Save;
 
 async fn healthz() -> &'static str {
     "OK!"
 }
 
-async fn upload(mut multipart: Multipart) -> Json<Value> {
+async fn to_json(mut multipart: Multipart) -> Json<Value> {
     if let Some(field) = multipart.next_field().await.unwrap() {
         let data = field.bytes().await.unwrap();
         let cursor = Cursor::new(data);
-        match Save::read(&mut BufReader::new(cursor)) {
+        match Save::read(&mut Box::new(BufReader::new(cursor))) {
             Ok(save) => Json(json!({ "data": save })),
             Err(_) => Json(json!({ "error": "Failed to parse .sav file" })),
         }
@@ -28,15 +28,68 @@ async fn upload(mut multipart: Multipart) -> Json<Value> {
     }
 }
 
+async fn from_json(Json(input): Json<Value>) 
+// -> Json<Value>
+-> impl IntoResponse
+// -> Result<bool, StatusCode>
+// -> Result<([(HeaderName, String); 2], Json<Value>), StatusCode> 
+{
+    // let mut writer = Box::new(BufWriter::new(
+    //     OpenOptions::new()
+    //         .create(true)
+    //         .truncate(true)
+    //         .write(true)
+    //         .open("tmp.sav")
+    //         .unwrap()
+    // ));
+
+    // //let save: Save = input;
+    // //save.write(&mut file)?;
+    // //Ok(file.buffer())
+
+    let s1: Save = Save::read(
+        &mut Box::new(
+            BufReader::new(
+                File::open(&"saves/Player_76561197994149843.sav").unwrap()
+            )
+        )
+    ).unwrap();
+
+    // let json2 = serde_json::to_value(s1).unwrap();
+
+    // let j1: Value = serde_json::from_reader(Box::new(BufReader::new(File::open(&"tmp.json").unwrap()))).unwrap();
+
+    // let j2: Save = serde_json::from_reader(Box::new(BufReader::new(File::open(&"tmp.json").unwrap()))).unwrap();
+
+    // let s2: Save = serde_json::from_value(json2.clone()).unwrap();
+
+    // let s3: Save = serde_json::from_value(input.clone()).unwrap();
+
+    // let mut file = Box::new(BufWriter::new(
+    //     OpenOptions::new()
+    //         .create(true)
+    //         .truncate(true)
+    //         .write(true)
+    //         .open("tmpCOOL.sav").unwrap()
+    // ));
+
+    let mut buffer: Vec<u8> = vec![];
+    s1.write(&mut buffer).unwrap();
+
+    buffer
+}
+
 #[shuttle_runtime::main]
 async fn main()-> shuttle_axum::ShuttleAxum {
     let cors = CorsLayer::new()
-        .allow_methods([Method::GET, Method::PUT])
+        .allow_methods([Method::OPTIONS, Method::GET, Method::PUT])
+        .allow_headers([axum::http::header::CONTENT_TYPE])
         .allow_origin(Any);
 
     let router = Router::new()
         .route("/", get(healthz))
-        .route("/upload", put(upload))
+        .route("/to_json", put(to_json))
+        .route("/from_json", put(from_json))
         .layer(cors);
 
     Ok(router.into())
--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -12,17 +12,49 @@ function App() {
     const [f] = acceptedFiles;
     if (!f) return;
 
+    if (!f.name.endsWith('.sav')) {
+      console.error('Invalid file type');
+      return;
+    }
+
     const formData = new FormData();
     formData.append('file', f);
 
-    fetch('http://0.0.0.0:8000/upload', {
+    fetch('http://localhost:8000/to_json', {
       method: 'PUT',
       body: formData,
-    }).then(r => r.text()).then(console.log);
+    }).then(r => r.json()).then(console.log);
+  }
+
+  function onDropJSON(acceptedFiles: File[]) {
+    const [f] = acceptedFiles;
+    if (!f) return;
+
+    fetch('http://localhost:8000/from_json', {
+      method: 'PUT',
+      body: JSON.stringify({}),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then(r => {
+      console.log('r', r);
+      return r.blob();  // Convert the response to a Blob
+    })
+      .then(blob => {
+        const url = window.URL.createObjectURL(blob);  // Create a URL for the Blob
+        const a = document.createElement('a');  // Create a <a> element
+        a.style.display = 'none';  // Hide the element
+        a.href = url;  // Set the download URL
+        a.download = 'file_name.ext';  // Set the file name and extension
+        document.body.appendChild(a);  // Append the element to the body
+        a.click();  // Programmatically click the link to trigger download
+        window.URL.revokeObjectURL(url);  // Revoke the object URL after download
+        document.body.removeChild(a);  // Clean up by removing the <a> element
+      })
   }
 
   function fetchme() {
-    fetch('http://0.0.0.0:8000/', {
+    fetch('http://localhost:8000/', {
       method: 'GET',
     }).then(r => r.text()).then(console.log);
   }
@@ -51,6 +83,16 @@ function App() {
               <div {...getRootProps()}>
                 <input {...getInputProps()} />
                 <p>Drag 'n' drop some files here, or click to select files</p>
+              </div>
+            </section>
+          )}
+        </Dropzone>
+        <Dropzone onDrop={onDropJSON} multiple={false}>
+          {({getRootProps, getInputProps}) => (
+            <section>
+              <div {...getRootProps()}>
+                <input {...getInputProps()} />
+                <p>DROP JSON TO SAV</p>
               </div>
             </section>
           )}
